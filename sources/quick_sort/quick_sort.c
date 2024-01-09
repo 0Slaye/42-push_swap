bb55ac7d9d1c9c5a571cf0e379cf1f11b8992ba3
0Slaye <uduterrage@gmail.com>
2024-01-09 17:51:13 +0100

    6 is the best for stacks

--- sources/quick_sort/quick_sort.c
+++ sources/quick_sort/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 16:32:59 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/01/09 17:34:16 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/01/09 17:50:35 by uwywijas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,31 @@ int	get_step(t_list **stack, int stacks)
 	return ((get_max(stack)->content + (get_min(stack)->content * -1)) / stacks);
 }
 
+void	sorter(t_list **stack_a, t_list **stack_b, t_list *max)
+{
+	int	is_reversed;
+
+	is_reversed = get_app_pos(stack_b, max);
+	if (is_reversed)
+	{
+		while (*stack_b != max)
+			rrb(stack_b);
+	}
+	else
+	{
+		while (*stack_b != max)
+			rb(stack_b);
+	}
+	pa(stack_a, stack_b);
+}
+
 void	quick_sort(t_list **stack_a, t_list **stack_b)
 {
 	t_list *holder;
 	int		stepper;
 	int		starter;
 
-	stepper = get_step(stack_a, 10);
+	stepper = get_step(stack_a, 6);
 	starter = get_min(stack_a)->content + stepper;
 	while (*stack_a)
 	{
@@ -40,5 +58,10 @@ void	quick_sort(t_list **stack_a, t_list **stack_b)
 				ra(stack_a);
 		}
 		starter += stepper;
+	}
+	while (*stack_b)
+	{
+		holder = get_max(stack_b);
+		sorter(stack_a, stack_b, holder);
 	}
 }
